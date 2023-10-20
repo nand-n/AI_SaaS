dfb1fc77e02d2c3eaad7aa155ca2d3a5ab44e666
Nahom <nahomdebele002@gmail.com>
2023-10-21 00:15:16 +0300

    added

--- app/(dashbord)/(routes)/dashbord/page.tsx
+++ app/(dashbord)/(routes)/dashbord/page.tsx
@@ -3,11 +3,11 @@ const dashbordPage =()=>{
   return (
 <div className="">
   <div className="flex justify-between items-center w-full h-full px-2">
-    <h1>Dashboard</h1>
+    <h1 className="ml-12">Dashboard</h1>
     <div className="w-[40px] h-[40px] bg-slate-300 rounded-full shadow-2xl"></div>
     </div>  
     <div className="grid grid-cols-12 items-center pr-4">
-      <div className="col-span-8 col-start-1 flex justify-evenly items-center">
+      <div className="col-span-8 col-start-1 flex justify-normal gap-12 ml-12 items-center">
   <div className="mt-2 flex justify-evenly">
       <div className="bg-gray-100 rounded-3xl shadow-lg p-4">
         <div className="flex justify-between items-center gap-24">
@@ -32,11 +32,11 @@ const dashbordPage =()=>{
       </div>
       
       <div className="col-span-4 col-start-9">
-        <div className="w-full h-12 rounded-xl grid grid-cols-4 items-center  bg-gray-100">
-          <div className="col-span px-2 py-1">Weekly</div>
-          <div className="col-span px-2 py-1">Monthly</div>
-          <div className="col-span px-2 py-1">Yearly</div>
-          <div className="col-span px-2 py-1">Custom date</div>
+        <div className="w-full h-14 rounded-xl grid grid-cols-4 items-center  bg-gray-100">
+          <div className="col-span  text-center">Weekly</div>
+          <div className="col-span text-white text-center bg-black">Monthly</div>
+          <div className="col-span text-center">Yearly</div>
+          <div className="col-span  text-center">Custom date</div>
 
         </div>
       </div>
