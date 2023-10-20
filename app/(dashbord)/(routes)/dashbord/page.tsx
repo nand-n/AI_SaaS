42efe7e2a3ebb7f679638d721229053949eae066
Nahom <nahomdebele002@gmail.com>
2023-10-20 23:43:09 +0300

    added

--- app/(dashbord)/(routes)/dashbord/page.tsx
+++ app/(dashbord)/(routes)/dashbord/page.tsx
@@ -2,8 +2,9 @@
 const dashbordPage =()=>{
   return (
 <div className="">
-  <div className="flex justify-center items-center w-full h-full">
-    <h1>Dashboard one </h1>
+  <div className="flex justify-between items-center w-full h-full px-2">
+    <h1>Dashboard</h1>
+    <div className="w-[40px] h-[40px] bg-slate-300 rounded-full shadow-lg"></div>
     </div>    
 </div>
     )
